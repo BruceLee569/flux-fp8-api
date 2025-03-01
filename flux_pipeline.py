56f61666df653b10bccd19acb7cc2927b95a4cef
bruce <569139367@qq.com>
2025-03-02 02:36:23 +0800

    update

--- flux_pipeline.py
+++ flux_pipeline.py
@@ -625,7 +625,6 @@ class FluxPipeline:
         )
         t_vec = None
         # dispatch to gpu if offloaded
-        logger.info(f"111:{self.offload_flow}")
         if self.offload_flow:
             self.model.to(self.device_flux)
 
